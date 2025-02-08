f0420f9e60205b0ff0b8d71c073c4e9052664889
Jack Wenyoung <dlwxxxdlw@gmail.com>
2025-02-08 17:16:35 +0800

    fix(modules): fix the without required issue
    This commit contains the following changes:
        M modules/home-manager/default.nix
        M modules/nixos/default.nix

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,9 +1,9 @@
 # Add your reusable home-manager modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
 # These should be stuff you would like to share with others, not your personal configurations.
-{inputs, ...} :
+{inputs, pkgs, lib, config, ...} :
 let
   moduleNameSpace = "JackwyHMMods";
-  args = { inherit inputs moduleNameSpace; };
+  args = { inherit inputs pkgs lib config moduleNameSpace; };
 in 
 {
   # List your module files here
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,9 +1,9 @@
 # Add your reusable NixOS modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
 # These should be stuff you would like to share with others, not your personal configurations.
-{inputs, ...} :
+{inputs, pkgs, config, lib, ...} :
 let
   moduleNameSpace = "JackwySystemMods";
-  args = { inherit inputs moduleNameSpace; };
+  args = { inherit inputs pkgs config lib moduleNameSpace; };
 in 
 {
   # List your module files here
