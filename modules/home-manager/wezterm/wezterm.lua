dd2f04d6ffb8f85b95c5e2d9e84940f5bff7b490
Jack Wenyoung <dlwxxxdlw@gmail.com>
2025-02-08 20:01:06 +0800

    chore(wezterm): adjust config

--- modules/home-manager/wezterm/wezterm.lua
+++ modules/home-manager/wezterm/wezterm.lua
@@ -1,14 +1,25 @@
 local wezterm = require("wezterm")
+
 local config = {
-  color_scheme = "catppuccin Macchiate",
+  color_scheme = "Catppuccin Macchiato",
   font = wezterm.font("Maple Mono NF", { weight="Bold", italic=false }),
+  font_size = 18,
   hide_tab_bar_if_only_one_tab = true,
-  use_fancy_tab_bar = false,
-  window_decorations = "RESIZE",
+  use_fancy_tab_bar = true,
+  tab_bar_at_bottom = true,
+  -- window_decorations = "RESIZE",
   show_new_tab_button_in_tab_bar = false,
   window_background_opacity = 0.9,
   text_background_opacity = 0.9,
   adjust_window_size_when_changing_font_size = false,
+  enable_wayland = true,
+  enable_scroll_bar = true,
+  window_padding = {
+    top = 2,
+    right = 4,
+    left = 2,
+    bottom = 20,
+  },
 }
 
 
