309802ea7c3639b66c282c141f4a1af1ee527874
Jack Wenyoung <dlwxxxdlw@gmail.com>
2025-02-16 17:39:13 +0800

    chore(qutebrowser): font, color and etc settings
    This commit contains the following changes:
        M modules/home-manager/qutebrowser.nix

--- modules/home-manager/qutebrowser.nix
+++ modules/home-manager/qutebrowser.nix
@@ -11,33 +11,47 @@ in {
     home.packages = with pkgs; [qutebrowser];
     programs.qutebrowser = {
       enable = true;
+      settings = {
+        zoom = {
+          default = "135%";
+        };
+        fonts = {
+          default_size = "16pt";
+          default_family = "Maple Mono NF";
+        };
+        colors = {
+          webpage = {
+            darkmode = {
+              enabled = true;
+              algorithm = "lightness-cielab";
+              contrast = 0.6;
+            };
+          };
+        };
+      };
       greasemonkey = [
-        (pkgs.fetchurl {
-          url = "https://update.greasyfork.org/scripts/512931/Dark%20Reader.user.js";
-          sha256 = "961508e128fc078075705a4f9d0c709b1ce8f4940e4a41b6353a6cb265b768ce";
-        })
-        (pkgs.writeText "dark-reader-unofficial.js" ''
-          // ==UserScript==
-          // @name          Dark Reader (Unofficial)
-          // @icon          https://darkreader.org/images/darkreader-icon-256x256.png
-          // @namespace     DarkReader
-          // @description	  Inverts the brightness of pages to reduce eye strain
-          // @version       4.7.15
-          // @author        https://github.com/darkreader/darkreader#contributors
-          // @homepageURL   https://darkreader.org/ | https://github.com/darkreader/darkreader
-          // @run-at        document-end
-          // @grant         none
-          // @include       http*
-          // @require       https://cdn.jsdelivr.net/npm/darkreader/darkreader.min.js
-          // @noframes
-          // ==/UserScript==
-          
-          DarkReader.enable({
-              brightness: 90,
-              contrast: 70,
-              sepia: 0
-          });
-        '')
+        # (pkgs.writeText "dark-reader-unofficial.js" ''
+        #   // ==UserScript==
+        #   // @name          Dark Reader (Unofficial)
+        #   // @icon          https://darkreader.org/images/darkreader-icon-256x256.png
+        #   // @namespace     DarkReader
+        #   // @description	  Inverts the brightness of pages to reduce eye strain
+        #   // @version       4.7.15
+        #   // @author        https://github.com/darkreader/darkreader#contributors
+        #   // @homepageURL   https://darkreader.org/ | https://github.com/darkreader/darkreader
+        #   // @run-at        document-end
+        #   // @grant         none
+        #   // @include       http*
+        #   // @require       https://cdn.jsdelivr.net/npm/darkreader/darkreader.min.js
+        #   // @noframes
+        #   // ==/UserScript==
+        #   
+        #   DarkReader.enable({
+        #       brightness: 90,
+        #       contrast: 70,
+        #       sepia: 0
+        #   });
+        # '')
       ];
     };
   };
