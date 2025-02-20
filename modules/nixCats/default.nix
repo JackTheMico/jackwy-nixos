5a1d118038e732582e712b81c845a4ae5f4fe0fa
Jack Wenyoung <dlwxxxdlw@gmail.com>
2025-02-20 12:16:45 +0800

    chore(nixCats): keep setup my nixCats
    This commit contains the following changes:
        M modules/nixCats/default.nix
        M modules/nixCats/lua/jackwy/plugins/blink.lua
        M modules/nixCats/lua/jackwy/plugins/init.lua

--- modules/nixCats/default.nix
+++ modules/nixCats/default.nix
@@ -1,6 +1,6 @@
-# Copyright (c) 2023 BirdeeHub 
-# Licensed under the MIT license 
-/* 
+# Copyright (c) 2023 BirdeeHub
+# Licensed under the MIT license
+/*
   # paste the inputs you don't have in this set into your main system flake.nix
   # (lazy.nvim wrapper only works on unstable)
   inputs = {
@@ -17,8 +17,13 @@
   from your system flake so that you can still run it via nix run from anywhere.
 
   The following is just the outputs function from the flake template.
- */
-{inputs, flake-path ? "/home/jackwenyoung/codes/jackwy/jackwy-nixos", ... }@attrs: let
+*/
+{
+  inputs,
+  flake-path ? "/home/jackwenyoung/codes/jackwy/jackwy-nixos",
+  ...
+}@attrs:
+let
   inherit (inputs) nixpkgs; # <-- nixpkgs = inputs.nixpkgsSomething;
   inherit (inputs.nixCats) utils;
   luaPath = "${./.}";
@@ -31,216 +36,238 @@
   extra_pkg_config = {
     allowUnfree = true;
   };
-  dependencyOverlays = /* (import ./overlays inputs) ++ */ [
-    # see :help nixCats.flake.outputs.overlays
-    # This overlay grabs all the inputs named in the format
-    # `plugins-<pluginName>`
-    # Once we add this overlay to our nixpkgs, we are able to
-    # use `pkgs.neovimPlugins`, which is a set of our plugins.
-    (utils.standardPluginOverlay inputs)
-    # add any flake overlays here.
+  dependencyOverlays = # (import ./overlays inputs) ++
+    [
+      # see :help nixCats.flake.outputs.overlays
+      # This overlay grabs all the inputs named in the format
+      # `plugins-<pluginName>`
+      # Once we add this overlay to our nixpkgs, we are able to
+      # use `pkgs.neovimPlugins`, which is a set of our plugins.
+      (utils.standardPluginOverlay inputs)
+      # add any flake overlays here.
 
-    # when other people mess up their overlays by wrapping them with system,
-    # you may instead call this function on their overlay.
-    # it will check if it has the system in the set, and if so return the desired overlay
-    # (utils.fixSystemizedOverlay inputs.codeium.overlays
-    #   (system: inputs.codeium.overlays.${system}.default)
-    # )
-  ];
+      # when other people mess up their overlays by wrapping them with system,
+      # you may instead call this function on their overlay.
+      # it will check if it has the system in the set, and if so return the desired overlay
+      # (utils.fixSystemizedOverlay inputs.codeium.overlays
+      #   (system: inputs.codeium.overlays.${system}.default)
+      # )
+    ];
 
-  categoryDefinitions = { pkgs, settings, categories, extra, name, mkNvimPlugin, ... }@packageDef: {
+  categoryDefinitions =
+    {
+      pkgs,
+      settings,
+      categories,
+      extra,
+      name,
+      mkNvimPlugin,
+      ...
+    }@packageDef:
+    {
 
-    lspsAndRuntimeDeps = with pkgs; {
-      general = {
-        core = [
-          universal-ctags
-          ripgrep
-          fd
-          ast-grep
-          lazygit
+      lspsAndRuntimeDeps = with pkgs; {
+        general = {
+          core = [
+            universal-ctags
+            ripgrep
+            fd
+            ast-grep
+            lazygit
+          ];
+          other = [
+            sqlite
+          ];
+          markdown = [
+            marksman
+            python312Packages.pylatexenc
+            harper
+          ];
+        };
+        lua = [
+          lua-language-server
+          stylua
+        ];
+        nix = [
+          nix-doc
+          nil
+          nixd
+          nixfmt-rfc-style
+        ];
+        neonixdev = [
+          nix-doc
+          nil
+          lua-language-server
+          nixd
+          nixfmt-rfc-style
+        ];
+        python = [
+          basedpyright
+          isort
+          ruff
+        ];
+      };
+
+      startupPlugins = with pkgs.vimPlugins; {
+        theme = builtins.getAttr (extra.colorscheme or "catppuccin-mocha") {
+          "onedark" = onedark-nvim;
+          "catppuccin" = catppuccin-nvim;
+          "catppuccin-mocha" = catppuccin-nvim;
+          "tokyonight" = tokyonight-nvim;
+          "tokyonight-day" = tokyonight-nvim;
+        };
+        general = [
+          lze
+          vim-repeat
+          nvim-nio
+          nvim-notify
+          nvim-web-devicons
+          nvim-lspconfig
+          plenary-nvim
+          mini-nvim
+          snacks-nvim
+        ];
+        lua = [
+          luvit-meta
+        ];
+        neonixdev = [
+          luvit-meta
+        ];
+        treesitter = [
+          nvim-treesitter-textobjects
+          nvim-treesitter.withAllGrammars
+          # (nvim-treesitter.withPlugins (
+          #   plugins: with plugins; [
+          #     bash
+          #     fish
+          #     nix
+          #     lua
+          #     python
+          #     markdown
+          #     go
+          #     javascript
+          #     html
+          #     css
+          #     astro
+          #     yaml
+          #     toml
+          #     json
+          #   ]
+          # ))
+        ];
+
+      };
+
+      optionalPlugins = with pkgs.vimPlugins; {
+        general = {
+          markdown = [
+            render-markdown-nvim
+            markdown-preview-nvim
+          ];
+          core = [
+            fzf-lua
+            nvim-surround
+            conform-nvim
+            undotree
+            vim-sleuth
+            lualine-lsp-progress
+            lualine-nvim
+            yazi-nvim
+            pkgs.neovimPlugins.houdini
+            bufferline-nvim
+            treesj
+          ];
+          cmp = [
+            blink-cmp
+            luasnip
+            friendly-snippets
+          ];
+        };
+        otter = [
+          otter-nvim
+        ];
+        python = [
+          nvim-dap-python
+        ];
+        neonixdev = [
+          lazydev-nvim
+        ];
+        debug = [
+          nvim-dap
+          nvim-dap-ui
+          nvim-dap-virtual-text
         ];
         other = [
-          sqlite
-        ];
-        markdown = [
-          marksman
-          python312Packages.pylatexenc
-          harper
-        ];
-      };
-      lua = [
-        lua-language-server
-        stylua
-      ];
-      nix = [
-        nix-doc
-        nil
-        nixd
-        nixfmt-rfc-style
-      ];
-      neonixdev = [
-        nix-doc
-        nil
-        lua-language-server
-        nixd
-        nixfmt-rfc-style
-      ];
-      python = [
-        basedpyright
-        isort
-        ruff
-      ];
-    };
-
-    startupPlugins = with pkgs.vimPlugins; {
-      theme = builtins.getAttr (extra.colorscheme or "catppuccin-mocha") {
-        "onedark" = onedark-nvim;
-        "catppuccin" = catppuccin-nvim;
-        "catppuccin-mocha" = catppuccin-nvim;
-        "tokyonight" = tokyonight-nvim;
-        "tokyonight-day" = tokyonight-nvim;
-      };
-      general = [
-        lze
-        vim-repeat
-        nvim-nio
-        nvim-notify
-        nvim-web-devicons
-        nvim-lspconfig
-        plenary-nvim
-        mini-nvim
-        snacks-nvim
-      ];
-      lua = [
-        luvit-meta
-      ];
-      neonixdev = [
-        luvit-meta
-      ];
-      treesitter = [
-        nvim-treesitter-textobjects
-        (nvim-treesitter.withPlugins (
-          plugins: with plugins; [
-            bash
-            fish
-            nix
-            lua
-            python
-            markdown
-            go
-            javascript
-            html
-            css
-            astro
-            yaml
-            toml
-            json
-          ]
-        ))
-      ];
-
-    };
-
-    optionalPlugins = with pkgs.vimPlugins; {
-      general = {
-        markdown = [
-          render-markdown-nvim
-          markdown-preview-nvim
-        ];
-        core = [
-          fzf-lua
+          pkgs.neovimPlugins.hlargs
+          todo-comments-nvim
           which-key-nvim
-          nvim-surround
-          conform-nvim
-          undotree
-          vim-sleuth
-          lualine-lsp-progress
-          lualine-nvim
-          yazi-nvim
-          pkgs.neovimPlugins.houdini
-          bufferline-nvim
-          treesj
+          nvim-highlight-colors
         ];
-        cmp = [
-          blink-cmp
-          luasnip
-          friendly-snippets
-        ];
+
       };
-      otter = [
-        otter-nvim
-      ];
-      python = [
-        nvim-dap-python
-      ];
-      neonixdev = [
-        lazydev-nvim
-      ];
-      debug = [
-        nvim-dap
-        nvim-dap-ui
-        nvim-dap-virtual-text
-      ];
-      other = [
-        pkgs.neovimPlugins.hlargs
-      ];
+
+      # shared libraries to be added to LD_LIBRARY_PATH
+      # variable available to nvim runtime
+      sharedLibraries = { };
+
+      environmentVariables = { };
+
+      extraWrapperArgs = { };
+      # https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/setup-hooks/make-wrapper.sh
+
+      # lists of the functions you would have passed to
+      # python.withPackages or lua.withPackages
+
+      # get the path to this python environment
+      # in your lua config via
+      # vim.g.python3_host_prog
+      # or run from nvim terminal via :!<packagename>-python3
+      extraPython3Packages = {
+        python = (
+          py: [
+            py.debugpy
+            py.pylsp-mypy
+            py.pyls-isort
+            py.pytest
+          ]
+        );
+      };
+      # populates $LUA_PATH and $LUA_CPATH
+      extraLuaPackages = {
+        test = [ (_: [ ]) ];
+      };
 
     };
 
-    # shared libraries to be added to LD_LIBRARY_PATH
-    # variable available to nvim runtime
-    sharedLibraries = {};
-
-    environmentVariables = {};
-
-    extraWrapperArgs = {};
-    # https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/setup-hooks/make-wrapper.sh
-
-    # lists of the functions you would have passed to
-    # python.withPackages or lua.withPackages
-
-    # get the path to this python environment
-    # in your lua config via
-    # vim.g.python3_host_prog
-    # or run from nvim terminal via :!<packagename>-python3
-    extraPython3Packages = {
-      python = (py:[
-        py.debugpy
-        py.pylsp-mypy
-        py.pyls-isort
-	py.pytest
-      ]);
-    };
-    # populates $LUA_PATH and $LUA_CPATH
-    extraLuaPackages = {
-      test = [ (_:[]) ];
+  jackwyvim_settings =
+    { pkgs, ... }@misc:
+    {
+      wrapRc = true;
+      withNodeJs = true;
+      withPython3 = true;
+      viAlias = false;
+      vimAlias = false;
+      unwrappedCfgPath = "${flake-path}/modules/nixCats";
     };
 
-  };
+  jackwyvim_categories =
+    { pkgs, ... }@misc:
+    {
+      theme = true;
+      general = true;
+      otter = true;
+      lua = true;
+      python = true;
+      neonixdev = true;
+      debug = true;
+      treesitter = true;
+    };
 
-  jackwyvim_settings = { pkgs, ...}@misc: {
-    wrapRc = true;
-    withNodeJs = true;
-    withPython3 = true;
-    viAlias = false;
-    vimAlias = false;
-    unwrappedCfgPath = "${flake-path}/modules/nixCats";
-  };
-
-  jackwyvim_categories = { pkgs, ...}@misc: {
-    theme = true;
-    general = true;
-    otter = true;
-    lua = true;
-    python = true;
-    neonixdev = true;
-    debug = true;
-    treesitter = true;
-  };
-
-  jackwyvim_extra = { pkgs, ...}@misc: {
-    colorscheme = "catppuccin-mocha";
-  };
+  jackwyvim_extra =
+    { pkgs, ... }@misc:
+    {
+      colorscheme = "catppuccin-mocha";
+    };
 
   packageDefinitions = {
     # NOTE: args allow to override attributes set.
@@ -272,19 +299,27 @@
   };
   # In this section, the main thing you will need to do is change the default package name
   # to the name of the packageDefinitions entry you wish to use as the default.
-    defaultPackageName = "ncat";
+  defaultPackageName = "ncat";
 in
-  # see :help nixCats.flake.outputs.exports
-  forEachSystem (system: let
+# see :help nixCats.flake.outputs.exports
+forEachSystem (
+  system:
+  let
     nixCatsBuilder = utils.baseBuilder luaPath {
-      inherit system dependencyOverlays extra_pkg_config nixpkgs;
+      inherit
+        system
+        dependencyOverlays
+        extra_pkg_config
+        nixpkgs
+        ;
     } categoryDefinitions packageDefinitions;
     defaultPackage = nixCatsBuilder defaultPackageName;
     # this is just for using utils such as pkgs.mkShell
     # The one used to build neovim is resolved inside the builder
     # and is passed to our categoryDefinitions and packageDefinitions
     pkgs = import nixpkgs { inherit system; };
-  in {
+  in
+  {
     # this will make a package out of each of the packageDefinitions defined above
     # and set the default package to the one passed in here.
     packages = utils.mkAllWithDefault defaultPackage;
@@ -296,39 +331,63 @@ in
         name = defaultPackageName;
         packages = [ defaultPackage ];
         inputsFrom = [ ];
-        shellHook = ''
-        '';
+        shellHook = '''';
       };
     };
 
-  }) // (let
+  }
+)
+// (
+  let
     # we also export a nixos module to allow reconfiguration from configuration.nix
     nixosModule = utils.mkNixosModules {
-      inherit defaultPackageName dependencyOverlays luaPath
-        categoryDefinitions packageDefinitions extra_pkg_config nixpkgs;
-      moduleNamespace = [ "jackwySystemMods" defaultPackageName];
+      inherit
+        defaultPackageName
+        dependencyOverlays
+        luaPath
+        categoryDefinitions
+        packageDefinitions
+        extra_pkg_config
+        nixpkgs
+        ;
+      moduleNamespace = [
+        "jackwySystemMods"
+        defaultPackageName
+      ];
     };
     # and the same for home manager
     homeModule = utils.mkHomeModules {
-      inherit defaultPackageName dependencyOverlays luaPath
-        categoryDefinitions packageDefinitions extra_pkg_config nixpkgs;
-      moduleNamespace = [ "jackwyHMMods" defaultPackageName];
+      inherit
+        defaultPackageName
+        dependencyOverlays
+        luaPath
+        categoryDefinitions
+        packageDefinitions
+        extra_pkg_config
+        nixpkgs
+        ;
+      moduleNamespace = [
+        "jackwyHMMods"
+        defaultPackageName
+      ];
     };
-  in {
+  in
+  {
 
-  # these outputs will be NOT wrapped with ${system}
+    # these outputs will be NOT wrapped with ${system}
 
-  # this will make an overlay out of each of the packageDefinitions defined above
-  # and set the default overlay to the one named here.
-  overlays = utils.makeOverlays luaPath {
-    # we pass in the things to make a pkgs variable to build nvim with later
-    inherit nixpkgs dependencyOverlays extra_pkg_config;
-    # and also our categoryDefinitions
-  } categoryDefinitions packageDefinitions defaultPackageName;
+    # this will make an overlay out of each of the packageDefinitions defined above
+    # and set the default overlay to the one named here.
+    overlays = utils.makeOverlays luaPath {
+      # we pass in the things to make a pkgs variable to build nvim with later
+      inherit nixpkgs dependencyOverlays extra_pkg_config;
+      # and also our categoryDefinitions
+    } categoryDefinitions packageDefinitions defaultPackageName;
 
-  nixosModules.default = nixosModule;
-  homeModules.default = homeModule;
+    nixosModules.default = nixosModule;
+    homeModules.default = homeModule;
 
-  inherit utils nixosModule homeModule;
-  inherit (utils) templates;
-})
+    inherit utils nixosModule homeModule;
+    inherit (utils) templates;
+  }
+)
--- modules/nixCats/lua/jackwy/plugins/init.lua
+++ modules/nixCats/lua/jackwy/plugins/init.lua
@@ -8,4 +8,148 @@ require("lze").load({
 	{ import = "jackwy.plugins.snacks" },
 	{ import = "jackwy.plugins.blink" },
 	{ import = "jackwy.plugins.nestsitter" },
+	{
+		"treesj",
+		for_cat = "general.core",
+		cmd = { "TSJToggle" },
+		keys = { { "<leader>Ft", ":TSJToggle<CR>", mode = { "n" }, desc = "treesj split/join" } },
+		after = function(plugin)
+			local tsj = require("treesj")
+
+			-- local langs = {--[[ configuration for languages ]]}
+
+			tsj.setup({
+				---@type boolean Use default keymaps (<space>m - toggle, <space>j - join, <space>s - split)
+				use_default_keymaps = true,
+				---@type boolean Node with syntax error will not be formatted
+				check_syntax_error = true,
+				---If line after join will be longer than max value,
+				---@type number If line after join will be longer than max value, node will not be formatted
+				max_join_length = 120,
+				---Cursor behavior:
+				---hold - cursor follows the node/place on which it was called
+				---start - cursor jumps to the first symbol of the node being formatted
+				---end - cursor jumps to the last symbol of the node being formatted
+				---@type 'hold'|'start'|'end'
+				cursor_behavior = "hold",
+				---@type boolean Notify about possible problems or not
+				notify = true,
+				---@type boolean Use `dot` for repeat action
+				dot_repeat = true,
+				---@type nil|function Callback for treesj error handler. func (err_text, level, ...other_text)
+				on_error = nil,
+				---@type table Presets for languages
+				-- langs = langs, -- See the default presets in lua/treesj/langs
+			})
+		end,
+	},
+	{
+		"otter.nvim",
+		for_cat = "otter",
+		-- event = "DeferredUIEnter",
+		on_require = { "otter" },
+		-- ft = { "markdown", "norg", "templ", "nix", "javascript", "html", "typescript", },
+		after = function(plugin)
+			local otter = require("otter")
+			otter.setup({
+				lsp = {
+					-- `:h events` that cause the diagnostics to update. Set to:
+					-- { "BufWritePost", "InsertLeave", "TextChanged" } for less performant
+					-- but more instant diagnostic updates
+					diagnostic_update_events = { "BufWritePost" },
+					-- function to find the root dir where the otter-ls is started
+					root_dir = function(_, bufnr)
+						return vim.fs.root(bufnr or 0, {
+							".git",
+							"_quarto.yml",
+							"package.json",
+						}) or vim.fn.getcwd(0)
+					end,
+				},
+				buffers = {
+					-- if set to true, the filetype of the otterbuffers will be set.
+					-- otherwise only the autocommand of lspconfig that attaches
+					-- the language server will be executed without setting the filetype
+					set_filetype = false,
+					-- write <path>.otter.<embedded language extension> files
+					-- to disk on save of main buffer.
+					-- usefule for some linters that require actual files
+					-- otter files are deleted on quit or main buffer close
+					write_to_disk = false,
+				},
+				verbose = { -- set to false to disable all verbose messages
+					no_code_found = false, -- warn if otter.activate is called, but no injected code was found
+				},
+				strip_wrapping_quote_characters = { "'", '"', "`" },
+				-- otter may not work the way you expect when entire code blocks are indented (eg. in Org files)
+				-- When true, otter handles these cases fully.
+				handle_leading_whitespace = false,
+			})
+		end,
+	},
+	{
+		"undotree",
+		for_cat = "general.core",
+		cmd = { "UndotreeToggle", "UndotreeHide", "UndotreeShow", "UndotreeFocus", "UndotreePersistUndo" },
+		keys = { { "<leader>U", "<cmd>UndotreeToggle<CR>", mode = { "n" }, desc = "Undo Tree" } },
+		before = function(_)
+			vim.g.undotree_WindowLayout = 1
+			vim.g.undotree_SplitWidth = 40
+		end,
+	},
+	{
+		"todo-comments.nvim",
+		for_cat = "other",
+		event = "DeferredUIEnter",
+		after = function(plugin)
+			require("todo-comments").setup({ signs = false })
+		end,
+	},
+	{
+		"hlargs",
+		for_cat = "other",
+		event = "DeferredUIEnter",
+		after = function(plugin)
+			require("hlargs").setup({
+				color = "#32a88f",
+			})
+			vim.cmd([[hi clear @lsp.type.parameter]])
+			vim.cmd([[hi link @lsp.type.parameter Hlargs]])
+		end,
+	},
+	{
+		"vim-sleuth",
+		for_cat = "general.core",
+		event = "DeferredUIEnter",
+	},
+	{
+		"nvim-highlight-colors",
+		for_cat = "other",
+		event = "DeferredUIEnter",
+		-- ft = "",
+		after = function(plugin)
+			require("nvim-highlight-colors").setup({
+				---Render style
+				---@usage 'background'|'foreground'|'virtual'
+				render = "virtual",
+
+				---Set virtual symbol (requires render to be set to 'virtual')
+				virtual_symbol = "■",
+
+				---Highlight named colors, e.g. 'green'
+				enable_named_colors = true,
+
+				---Highlight tailwind colors, e.g. 'bg-blue-500'
+				enable_tailwind = true,
+
+				---Set custom colors
+				---Label must be properly escaped with '%' to adhere to `string.gmatch`
+				--- :help string.gmatch
+				custom_colors = {
+					{ label = "%-%-theme%-primary%-color", color = "#0f1219" },
+					{ label = "%-%-theme%-secondary%-color", color = "#5a5d64" },
+				},
+			})
+		end,
+	},
 })
