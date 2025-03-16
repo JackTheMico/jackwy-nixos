9bf94b995a11d55ec4951adb9238e0cb19c53051
Jack Wenyoung <dlwxxxdlw@gmail.com>
2025-03-16 16:06:46 +0800

    monitor scale fix

--- home-manager/jackwy/nixos-jackwy-desktop.nix
+++ home-manager/jackwy/nixos-jackwy-desktop.nix
@@ -37,7 +37,7 @@
   jackwyHMMods.hyprland = {
     enable = true;
     autoEnter = false;
-    monitor = ["DP-2, 1920x1080@60, 0x0, 1" "DP-3, 2560x1440@60, 1920x0, 1.20"];
+    monitor = ["DP-2, 1920x1080@60, 0x0, 1" "DP-3, 2560x1440@60, 1920x0, 1"];
   };
   jackwyHMMods.ssh = {
     enable = true;
