ffae4b68acee4078f92cd9dae2759dbbe58d2994
Jack Wenyoung <dlwxxxdlw@gmail.com>
2025-03-26 20:23:03 +0800

    try to make docker and dynamically executable work

--- modules/nixos/docker.nix
+++ modules/nixos/docker.nix
@@ -13,13 +13,43 @@ in {
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [arion compose2nix lazydocker docker-client];
-    virtualisation.docker.enable = false;
-    virtualisation.podman = {
+    environment.systemPackages = with pkgs; [arion compose2nix lazydocker docker-compose];
+    # environment.etc."containers/registries.conf".text = lib.mkForce ''
+    #   unqualified-search-registries = ["docker.io"]
+    #
+    #   # Docker Hub 镜像加速
+    #   [[registry]]
+    #   prefix = "docker.io"
+    #   location = "docker.io"
+    #   [[registry.mirror]]
+    #   location = "xxxx.mirror.aliyuncs.com"  # 阿里云
+    #   [[registry.mirror]]
+    #   location = "docker.mirrors.ustc.edu.cn"  # 中科大
+    #
+    #   # Red Hat 镜像配置
+    #   [[registry]]
+    #   prefix = "registry.redhat.io"
+    #   location = "mirror.registry.redhat.example.com"  # 企业内网镜像
+    #   insecure = true  # 若使用自签名证书需启用
+    # '';
+    virtualisation.docker = {
       enable = true;
-      dockerSocket.enable = true;
-      defaultNetwork.settings.dns_enabled = true;
+      daemon.settings = {
+        "registry-mirrors" = [
+          "https://registry.docker-cn.com" # Docker 中国官方镜像（可能已失效，需替换）
+          "https://mirror.baidubce.com" # 百度云镜像
+        ];
+      };
     };
+    programs.nix-ld = {
+      enable = true;
+      # libraries = with pkgs; [];
+    };
+    # virtualisation.podman = {
+    #   enable = true;
+    #   dockerSocket.enable = true;
+    #   defaultNetwork.settings.dns_enabled = true;
+    # };
     users.users.${userName}.extraGroups = ["docker" "podman"];
   };
 }
