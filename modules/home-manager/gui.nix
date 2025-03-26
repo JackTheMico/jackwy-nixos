4f0e39dbbc51b6c8ceb568522931b77c69e7e0c2
Jack Wenyoung <dlwxxxdlw@gmail.com>
2025-03-26 18:12:52 +0800

    fix zoom share screen

--- modules/home-manager/gui.nix
+++ modules/home-manager/gui.nix
@@ -6,21 +6,31 @@
 }:
 with lib; let
   cfg = config.${moduleNameSpace}.gui;
+  pinnedZoomPkgs =
+    import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/0c19708cf035f50d28eb4b2b8e7a79d4dc52f6bb.tar.gz";
+      sha256 = "0ngw2shvl24swam5pzhcs9hvbwrgzsbcdlhpvzqc7nfk8lc28sp3";
+    }) {
+      system = "x86_64-linux";
+      config.allowUnfree = true;
+    };
 in {
   options.${moduleNameSpace}.gui = {
     enable = mkEnableOption "User GUI Enable";
   };
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [
-      discord
-      freetube
-      keepassxc
-      spotify
-      qq
-      wechat-uos
-      zoom-us
-    ];
+    home.packages = with pkgs;
+      [
+        discord
+        freetube
+        keepassxc
+        spotify
+        qq
+        wechat-uos
+        # zoom-us
+      ]
+      ++ [pinnedZoomPkgs.zoom-us];
     programs = {
       freetube = {
         enable = true;
@@ -35,5 +45,17 @@ in {
         plugins = with pkgs.obs-studio-plugins; [wlrobs obs-shaderfilter input-overlay waveform];
       };
     };
+    # xdg.portal = {
+    #   extraPortals = with pkgs; [
+    #     xdg-desktop-portal-hyprland
+    #   ];
+    #   config.hyprland = {
+    #     "org.freedesktop.impl.portal.ScreenCast" = "hyprland";
+    #   };
+    # };
+    # xdg.configFile."zoomus.conf".text = ''
+    #   enableWaylandShare=true
+    #   xwayland=true
+    # '';
   };
 }
